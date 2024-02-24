20dc32b165895bdc615dbbffbabf747bbfe1ae00
Payalsasmal10 <sasmalpayal@gmail.com>
2024-02-24 14:34:13 +0530

    creating header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,14 @@
-
 const Header = () => {
-    
+  return (
+    <section>
+      <div className="header-div">
+        <h1>Software Engineer, Front End</h1>
+        <h2>at Peerlist • Full time • Remote (United States, Canada)</h2>
+        <span>78 Candidates</span>
+        <span>68 Applied w/ Peerlist</span>
+        <span>2872 Views</span>
+      </div>
+    </section>
+  );
 };
 export default Header;
